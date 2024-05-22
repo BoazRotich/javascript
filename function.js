8fd3e31517c84177d86c485226021f31eb8572d7
BoazRotich <boazkiprotich96@gmail.com>
2024-05-22 14:12:43 +0300

    stage changes

---
+++ function.js
@@ -0,0 +1,64 @@
+/*
+declaring of function : there are 3 methods of declaring a function:
+// function declaration: function name,parameters<function body=> accessed at global level 
+
+ function expression: a function declared and stored in a variable:classes,local scope arrow function:for ease and expertise accessibility
+
+naming conventions functions : camelcase,pascal ,snake
+
+camelcase : function getName
+pascal:function GET_NAME
+snake:function get_name
+*/
+
+// function declaration
+// void function no parameter
+
+
+// let firstname="John"
+// let secondname="Mwangi"
+
+// function getName(firstname,secondname){
+//     console.log(`${firstname} ${secondname}`);
+// }
+
+// getName(firstname,secondname)
+
+
+// function boaz(length,width){
+//     return(length*width)
+    
+// }
+// let length=20;
+// let width=12;
+// console.log(length*width)
+
+// fuction expression
+
+// let firstname="John"
+// let secondname="Mwangi"
+
+// let name=function getName(firstname,secondname){
+//     console.log(`${firstname} ${secondname}`)
+// }
+// name(firstname,secondname)
+
+// arrow function
+// short hand notation to variable naming,used in call back functions,embedded functions
+// simplicity
+
+// function name is replaced by the arrow symbol which is placed after the parameter bracket
+
+// let firstname="John"
+// let secondname='Mwangi'
+// let name=(firstname,secondname)=>{
+//     console.log(`${firstname} ${secondname}`)
+// }
+// name(firstname,secondname)
+
+
+// calculating perimeter of a triange
+
+function findPerimeter(side1,side2,side3){
+    
+}
---
+++ variables.js
@@ -0,0 +1,9 @@
+// What are variables
+
+
+
+
+//Passing by referrence vs passing by value (CS) 
+//variable naming rules in Javascript
+//Variables naming conventions(CS)
+//what is an API ---eg what daraj api used for?
